774434e97b5ba68bcbf13d1a8922475d8fa8b690
flaviodrummond <flaviosax20@gmail.com>
2024-01-17 10:44:50 -0300

    Finalizando a estilização da tela de pedidos

--- src/styles.js
+++ src/styles.js
@@ -16,21 +16,69 @@ margin-top: 11 px;
 `;
 
 export const ContainerItens = styled.div`
-
+display: flex;
+flex-direction: column;
 `;
 
 export const H1 = styled.h1`
-
+color: #FFF;
+text-align: center;
+font-family: Roboto;
+font-size: 28px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+margin-bottom: 76px;
 `;
 
 export const InputLabel = styled.p`
-
+color: #EEE;
+font-size: 18px;
+font-style: normal;
+font-weight: 700;
+line-height: 22px;
+margin-left: 15px;
 `;
 
 export const Input = styled.input`
+border-radius: 14px;
+background: rgba(255, 255, 255, 0.25);
+width: 342px;
+height: 58px;
+border: none;
+outline: none;
+padding: 15px;
+color: #FFF;
 
+font-size: 18px;
+font-style: normal;
+font-weight: 300;
+line-height: normal;
+margin-bottom: 45px;
 `;
 
 export const Button = styled.button`
+background: #D93856;
+width: 342px;
+height: 68px;
+padding: 0px 113px;
+justify-content: center;
+align-items: center;
+border: none;
+outline: none;
+margin-top: 60px;
 
+color: #FFF;
+text-align: center;
+font-size: 17px;
+font-style: normal;
+font-weight: 900;
+cursor: pointer;
+
+&:hover {
+    opacity: .8;
+}
+&:active {
+    opacity: .4;
+}
 `;
